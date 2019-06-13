910be3df2235c5c925d295f71f27d1fad6efdc07
diasflack <diasflack@xhamster.com>
2019-06-13 17:18:40 +0300

    - min metrics lines

--- plotvmaf.py
+++ plotvmaf.py
@@ -51,11 +51,6 @@ with open(args.input) as json_file:
         psnr_data.append(frame['metrics']['psnr'])
         ssim_data.append(frame['metrics']['ssim'])
 
-# end frame subprocess
-
-global_peak_bitrate = 0.0
-global_mean_bitrate = 0.0
-
 # render charts in order of expected decreasing size
 vmaf_array = numpy.array(vmaf_data)
 psnr_array = numpy.array(psnr_data)
@@ -104,6 +99,43 @@ ssim_ax.grid(True)
 vmaf_ax.plot(x, vmaf_y)
 psnr_ax.plot(x, psnr_y)
 ssim_ax.plot(x, ssim_y)
+
+# calculate min vmaf line position 
+vmaf_text_x = vmaf_ax.get_xlim()[1] * 0.15
+vmaf_text_y = vmaf_array.min() + \
+    ((vmaf_ax.get_ylim()[1] - vmaf_ax.get_ylim()[0]) * 0.015)
+vmaf_text = "vmaf min ({:})".format(vmaf_array.min())
+
+# draw peak as think black line w/ text
+vmaf_ax.axhline(vmaf_array.min(), linewidth=2, color='black')
+vmaf_ax.text(vmaf_text_x, vmaf_text_y, vmaf_text,
+    horizontalalignment='center', fontweight='bold', color='black')  
+
+# calculate min psnr line position 
+psnr_text_x = psnr_ax.get_xlim()[1] * 0.15
+psnr_text_y = psnr_array.min() + \
+    ((psnr_ax.get_ylim()[1] - psnr_ax.get_ylim()[0]) * 0.015)
+psnr_text = "psnr min ({:})".format(psnr_array.min())
+
+# draw peak as think black line w/ text
+psnr_ax.axhline(psnr_array.min(), linewidth=2, color='black')
+psnr_ax.text(psnr_text_x, psnr_text_y, psnr_text,
+    horizontalalignment='center', fontweight='bold', color='black')
+
+# calculate min ssim line position 
+ssim_text_x = ssim_ax.get_xlim()[1] * 0.15
+ssim_text_y = ssim_array.min() + \
+    ((ssim_ax.get_ylim()[1] - ssim_ax.get_ylim()[0]) * 0.015)
+ssim_text = "ssim min ({:})".format(ssim_array.min())
+
+# draw peak as think black line w/ text
+ssim_ax.axhline(ssim_array.min(), linewidth=2, color='black')
+ssim_ax.text(ssim_text_x, ssim_text_y, ssim_text,
+    horizontalalignment='center', fontweight='bold', color='black')
+
+fig.text(0.1, 0.93, vmaf_text, fontweight='bold', color='black')
+fig.text(0.1, 0.91, psnr_text, fontweight='bold', color='black')
+fig.text(0.1, 0.89, ssim_text, fontweight='bold', color='black')       
 
 # render graph to file (if requested) or screen
 if args.output:
